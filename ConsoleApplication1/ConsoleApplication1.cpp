a7d974bb947c475fa3399d23c49086c3c92b488d
EMBEDDED <kkt1803@naver.com>
2023-11-01 16:49:21 +0900

    20231101

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -31,34 +31,55 @@ public:
     int x;
     int y;
 
-    Point(){} // null argument 허용하는 생성자
+    //Point(){} // null argument 허용하는 생성자
 
-    Point(int x1, int y1)
+    Point(int x1 = 0, int y1 = 0)
     {
         x = x1; y = y1;
     }
 
     int Area(Point p);
+    int Area(Point p1, Point p2);
     double Dist(Point p);
+    double Dist(Point p1, Point p2);
+    double Dist(int x1, int y1);
 
 };
+
+double Point::Dist(Point p1, Point p2)
+{
+    int w = p1.x - p2.x;
+    int h = p1.y - p2.y;
+    double d2 = sqrt(w * w + h * h);
+
+    return d2;
+}
+
+int Point::Area(Point p1, Point p2)
+{
+    int w = ABS(p1.x - p2.x);
+    int h = ABS(p1.y - p2.y);
+    int a2 = w * h;
+
+    return a2;
+}
 
 double Point::Dist(Point p)
 {
     int w = x - p.x;
     int h = y - p.y;
-    double d = sqrt(w * w + h * h);
+    double d1 = sqrt(w * w + h * h);
 
-    return d;
+    return d1;
 }
 
 int Point::Area(Point p)
 {
     int w = ABS(x - p.x);
     int h = ABS(y - p.y);
-    int a = w * h;
+    int a1 = w * h;
 
-    return a;
+    return a1;
 }
 
 int main()
@@ -72,17 +93,19 @@ int main()
     printf("%d의 제곱 : %d\n", x ,SQUARE(x));
     printf("%d의 절댓값 : %d\n", x, ABS(x));*/
 
-    Point p1(10, 10), p2(20, 30);
-    /*p1.x = 10; p1.y = 10;
-    p2.x = 20; p2.y = 30;*/
+    Point p1(10, 10), p2(20, 30), p3;
 
-    double d = p1.Dist(p2);
-    printf("두 점 p1(%d, %d) p2(%d %d)의 거리는 %.2f 입니다.\n\n", 
-        p1.x, p1.y, p2.x, p2.y, d);
+    double d1 = p1.Dist(p2);
+    int a1 = p1.Area(p2);
+    double d2 = p1.Dist(p1, p2);
+    int a2 = p3.Area(p1, p2);
+    
+    printf("두 점 p1(%d, %d) p2(%d %d)의 거리는 %.2f, 면적은 %d 입니다.\r\n",
+        p1.x, p1.y, p2.x, p2.y, d1, a1);
+    printf("두 점 p1(%d, %d) p2(%d %d)의 거리는 %.2f, 면적은 %d 입니다.\r\n", 
+        p1.x, p1.y, p2.x, p2.y, d2, a2);
+    printf("null argument P3는 p(%d, %d)로 설정되었습니다.", p3.x, p3.y);
 
-    int a = p1.Area(p2);
-    printf("두 점 p1(%d, %d) p2(%d %d)의 넓이는 %d 입니다.\n",
-        p1.x, p1.y, p2.x, p2.y, a);
 }
 
 //double Dist(Point2D p1, Point2D p2) //두 점간의 거리
---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#define MIN(x, y)   (((x) > (y)) ? (y) : (x))
+#define MAX(x, y)   (((x) > (y)) ? (x) : (y))
+
+class Point
+{
+private:  // 접근 지시자(외부 참조 불가)
+
+public:  // 접근 지시자(외부 참조 가능)
+    
+    int x, y;  // 멤버 변수 선언
+
+    Point(int x1 = 0, int y1 = 0) //: x(x1 + 1), y(y1 + 1) // => initializer
+        // 생성자 정의. 함수적 특성(default, overload 등)
+    {
+        x = x1; y = y1;
+    }
+
+    double Dist(Point p)  // 멤버 함수 : 두 점간의 거리 계산. Double 값으로 return
+    {
+        int w = x - p.x;
+        int h = y - p.y;
+       
+        return sqrt((w * w) + (h * h)); // sqrt 함수 자료형이 double형이다
+    }
+
+    double Dist(Point p1, Point p2)  // 멤버 함수 : 두 점간의 거리 계산. Double 값으로 return
+    {
+        int w = p1.x - p2.x;
+        int h = p1.y - p2.y;
+
+        return sqrt((w * w) + (h * h)); // sqrt 함수 자료형이 double형이다
+    }
+};
+
+class Rect
+{
+public:
+    Point a, b; // 클래스 멤버변수 선언
+    //Point LL, LR, UL, UR; // 외부참조대상
+
+    Rect(Point p1, Point p2)
+    {
+        a = p1; b = p2;
+        /*int xlf = MIN(p1.x, p2.x);
+        int xrt = MAX(p1.x, p2.x);
+        int ylw = MIN(p1.y, p2.y);
+        int yup = MAX(p1.y, p2.y);
+        LL = Point(xlf, ylw);
+        LR = Point(xrt, ylw);
+        UL = Point(xlf, yup);
+        UR = Point(xrt, yup);*/
+    }
+
+    Point GetLL()
+    {
+        return Point(MIN(a.x, b.x), MIN(a.y, b.y));
+    }
+    Point GetLR()
+    {
+        return Point(MAX(a.x, b.x), MIN(a.y, b.y));
+    }
+    Point GetUL()
+    {
+        return Point(MIN(a.x, b.x), MAX(a.y, b.y));
+    }
+    Point GetUR()
+    {
+        return Point(MAX(a.x, b.x), MAX(a.y, b.y));
+    }
+};
+
+void printP(Point p)
+{
+    printf("Point(%d, %d)\n", p.x, p.y);
+}
+
+int main()
+{
+    Point p1, p2(10, 10), p3(20, 30), p4;
+    p4 = p3;
+    double d1 = p1.Dist(p2);
+    double d2 = p2.Dist(p3);
+    double d3 = p1.Dist(p3);
+
+    printP(p4);
+    printP(p1);
+    printP(p2);
+    printP(p3);
+    printf("점간의 거리 계산 : (p1, p2) = %.2f (p2, p3) = %.2f (p1, p3) = %.2f\n", d1, d2, d3);
+
+    Rect r(p2, p3);
+    printf("두 점\n");
+    printP(p2);
+    printP(p3);
+    printf("에 의해 정의된 사각형의 4점의 좌표는\n");
+    printP(r.GetLL());
+    printP(r.GetLR());
+    printP(r.GetUL());
+    printP(r.GetUR());
+}
