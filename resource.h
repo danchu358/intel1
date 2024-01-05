8d5312515ba46d34ca8d07a1052000ae2070a2b3
danchu358 <61377910+danchu358@users.noreply.github.com>
2024-01-05 17:04:23 +0900

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,58 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// mfcMemo.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCMEMO_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
+#define IDD_FIND_DIALOG                 132
+#define IDR_ACCEL1                      134
+#define IDD_REPLACE_DIALOG              137
+#define IDC_EDIT1                       1000
+#define IDC_EDIT_MEMO                   1000
+#define IDC_EDIT_REPLACE1               1001
+#define IDC_EDIT3                       1002
+#define IDC_EDIT_FIND                   1002
+#define IDC_COMBO1                      1003
+#define IDC_BUTTON_END                  1007
+#define IDC_EDIT_FIND1                  1008
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_MENU_NEW                     32775
+#define ID_32776                        32776
+#define ID_MENU_OPEN                    32777
+#define ID_MENU_SAVE                    32778
+#define ID_MENU_EXIT                    32779
+#define ID_MENU_ABOUT                   32780
+#define ID_32781                        32781
+#define ID_32782                        32782
+#define ID_MENU_FIND                    32783
+#define ID_                             32784
+#define ID_MENU_REPLACE                 32785
+#define ID_32786                        32786
+#define ID_32787                        32787
+#define ID_32788                        32788
+#define ID_MENU_FINDNEXT                32789
+#define ID_32793                        32793
+#define ID_32794                        32794
+#define ID_MENU_UTF8                    32795
+#define ID_MENU_ANSI                    32796
+#define ID_32799                        32799
+#define ID_32800                        32800
+#define ID_MENU_FONT                    32801
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        139
+#define _APS_NEXT_COMMAND_VALUE         32802
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
